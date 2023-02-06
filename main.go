b27b3a42fdca0ebb39483be267e21af4007af8e4
inciner8r <punarvpawade17@gmail.com>
2023-02-06 12:18:31 +0530

    cleanup

--- main.go
+++ main.go
@@ -33,22 +33,9 @@ func main() {
 	fmt.Println("here")
 }
 
-// generated with help of https://mholt.github.io/json-to-go/
 type links struct {
-	Name string `json:"name"`
-	// Path        string `json:"path"`
-	// Sha         string `json:"sha"`
-	// Size        int    `json:"size"`
-	// URL         string `json:"url"`
-	// HTMLURL     string `json:"html_url"`
-	// GitURL      string `json:"git_url"`
+	Name        string `json:"name"`
 	DownloadURL string `json:"download_url"`
-	// Type        string `json:"type"`
-	// Links       struct {
-	// 	Self string `json:"self"`
-	// 	Git  string `json:"git"`
-	// 	HTML string `json:"html"`
-	// } `json:"_links"`
 }
 
 func fetchLinks(link string) []links {
